2c82581c3e65a43c10b711a9ddaa461c032eea81
unknown <prakashbahadursingh2@gmail.com>
2020-07-13 15:34:35 -0700

    in the queries.sql file all the queries are checked on w3school.com and then added to the file

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const AccountsRouter = require("../routers/accounts-router.js")
 
-const db = require("../data/dbConfig.js");
+const AccountDb = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", AccountsRouter)
+
+server.get("/", (req, res) => {
+    res.status(200).json({message: "This is second week, day 1 back end project about DATABASE (db)"})
+})
 
 module.exports = server;
---
+++ routers/accounts-router.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+const AccountDb = require("../data/dbConfig.js");
+
+const router = express.Router()
+
+
+router.get("/", (req, res) => {
+    AccountDb.select("*")
+        .from("accounts")
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(error => {
+            handleError(error, res)
+
+            console.log(error)
+            res.status(500).json({message: "there is error getting accounts data"})
+        })
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+
+    AccountDb.from("accounts")
+        .where({ id })
+        .first()
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(error => {
+            handleError(error, res)
+        })
+})
+
+router.post("/", (req, res) => {
+    const postAccountData = req.body
+    //validate the data
+    AccountDb("accounts")
+        .insert(postAccountData, "id")
+        .then(ids => {
+            AccountDb("accounts")
+                .where({ id: ids[0] })
+                .first()
+                .then(accountInfo => {
+                    res.status(200).json(accountInfo)
+                })
+        })
+        .catch(error => {
+            handleError(error, res)
+        })
+})
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const change = req.body;
+
+    AccountDb("accounts")
+        .where({ id })// do not forget to have where here
+        .update(change)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({message: "there is no record to update"})
+            }
+        })
+        .catch(error => {
+            handleError(error, res)
+        })
+})
+
+
+
+
+
+
+function handleError(error, res) {
+    console.log("error", error);
+    res.status(500).json({ message: error.message });
+}
+
+module.exports = router
